42d553d1db3ad2cf1374eee448ffa43fefbb7eaf
VitasOnline <vitasonline@mail.ru>
2019-07-04 20:22:34 +0300

    new

--- gulpfile.js
+++ gulpfile.js
@@ -9,25 +9,25 @@ gulp.task('server', function() {
 
     browserSync({
         server: {
-            baseDir: "docs"
+           // baseDir: ""
         }
     });
 
-    gulp.watch("docs/*.html").on('change', browserSync.reload);
+    gulp.watch("*.html").on('change', browserSync.reload);
 });
 
 gulp.task('styles', function() {
-    return gulp.src("docs/sass/**/*.+(scss|sass)")
+    return gulp.src("sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("docs/css"))
+        .pipe(gulp.dest("css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("docs/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("sass/**/*.+(scss|sass)", gulp.parallel('styles'));
 })
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
